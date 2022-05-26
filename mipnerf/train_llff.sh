5342296e478efb87b231ed84d4f6bc8f96d0f42b
kzhang19 <65601837+zhangkai0425@users.noreply.github.com>
2022-05-26 14:38:09 +0800

    Add files via upload

---
+++ mipnerf/train_llff.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Script for training on the LLFF dataset.
+
+SCENE=trex
+EXPERIMENT=debug
+TRAIN_DIR=/data/guangyu/zhangkai/nerf_results/$EXPERIMENT/$SCENE
+DATA_DIR=/data/guangyu/zhangkai/nerf_llff_data/$SCENE
+
+unset LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/guangyu/miniconda3/envs/jaxnerf/lib/
+export XLA_FLAGS=--xla_gpu_cuda_data_dir=/usr/local/cuda-11.4
+
+rm $TRAIN_DIR/*
+CUDA_VISIBLE_DEVICES=0 python -m train \
+  --data_dir=$DATA_DIR \
+  --train_dir=$TRAIN_DIR \
+  --gin_file=configs/llff.gin \
+  --logtostderr
